040a2c8f53414fbafd190dc19f9932bc049185ff
Matheus Daniel <matheusdanielgomess@gmail.com>
2019-08-10 21:07:34 -0400

    página de cadastro adicionada

--- Cadastrar.php
+++ Cadastrar.php
@@ -5,11 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cadastrado &#x2015; IF Maker</title>
+    <meta name="application-name" content="IF Maker">
+    <meta name="author" content="br.edu.ifms">
+    <meta name="description" content="Página de cadastro do IF Maker">
+    <meta name="generator" content="Visual Studio Code">
+    <meta name="keywords" content="Laboratório, Prototipação, Arduíno">
     <link rel="stylesheet" type="text/css" href="./css/reset.css"> <!-- reset adaptado -->
     <link rel="stylesheet" type="text/css" href="./css/index.css">
     <link rel="stylesheet" type="text/css" href="./css/cadastrar.css">
     <link rel="shortcut icon" href="./favicon.ico">
+    <title>Cadastro concluído &#x2015; IF Maker</title>
 </head>
 
 <body>
@@ -25,38 +30,80 @@
             <summary>Dados do usuário</summary>
 
             <?php
-            $preenchido = isset($_POST["enviar"]);
+            function indentacao($N) {
+                for ($n = 0; $n < $N; ++$n)
+                    echo "\t";
+            }
+
+            $preenchido = isset($_POST["enviar"]) && strlen($_POST["nome"]) != 0
+                && strlen($_POST["email"]) != 0 && strlen($_POST["senha"]) != 0
+                && strlen($_POST["termos"]) != 0;
+
             if (!$preenchido)
                 header("Location: .");
 
-            echo "<ul class='cadastro'>";
+            echo "<ul class=\"cadastro\">\n";
 
+            $eventos = "";
             foreach ($_POST as $key => &$value) {
-                if ($key == "enviar" || $key == "senha")
+                if ($key == "senha" || $key == "enviar" || $key == "termos")
                     continue;
+
+                if (substr($key, 0, 6) == "evento") {
+                    $eventos .= $value . "; ";
+                    continue;
+                }
 
                 if ($key == "historico")
                     $value = str_replace("\n", "<br>", $value);
-
+                
                 if ($key == "dataNascimento" && strlen($value) != 0) {
                     $value = strtotime($value);
                     $value = date("d/m/Y", $value);
                 }
 
-                echo "<li><b>" . ucfirst($key) . ":</b> ";
+                $label = $key;
+                switch ($label) {
+                    case "dataNascimento":
+                        $label = "Data de nascimento";
+                        break;
+                    case "cpf":
+                        $label = "CPF";
+                        break;
+                    case "endereco":
+                        $label = "Endereço";
+                        break;
+                    case "numero":
+                        $label = "Número";
+                        break;
+                    case "cep":
+                        $label = "CEP";
+                        break;
+                    case "historico":
+                        $label = "Histórico";
+                        break;
+                    case "poc":
+                        $label = "Cursos/Palestras/Oficinas";
+                        break;
+                }
 
-                echo (strlen($value) == 0 ? "Não especificado(a)" : $value) . "</li>";
+                echo indentacao(4) . "<li><b>" . ucfirst($label) . ":</b> ";
+
+                echo (strlen($value) == 0 ? "Não especificado(a)" : $value) . "</li>\n";
             }
 
-            echo "</ul>";
+            if (strlen($eventos) != "")
+                echo indentacao(4) . "<li><b>Eventos:</b> " . $eventos . "</li>\n";
+
+            echo indentacao(3) . "</ul>\n";
             ?>
         </details>
     </main>
 
     <aside>
         <ul class="links">
-            <li><a href="#">Nossos serviços</a></li>
-            <li><a href="./../">Cadastro</a></li>
+            <li><a href=".">Nossos serviços</a></li>
+            <li><a href="./cadastrar.html">Cadastro</a></li>
             <li><a href="#">Quem somos</a></li>
             <li><a href="http://ifms.edu.br/" target="_blank">Site do IFMS</a></li>
         </ul>
---
+++ js/cadastrar.js
@@ -0,0 +1,66 @@
+function labelClicavel(event) {
+    if (event.target.tagName != "LABEL") return;
+
+    var label = {};
+    label.for = event.target.attributes.for.value;
+
+    var input = document.querySelector("input[name='" + label.for + "']");
+
+    if (input.attributes.type.value != "checkbox") return;
+
+    input.checked = !input.checked; // true -> false & vice-versa
+}
+
+var cpf = document.querySelector("input[name='cpf']");
+var celular = document.querySelector("input[name='celular']");
+var cep = document.querySelector("input[name='cep']");
+
+cpf.onkeyup = function (event) {
+    if (event.key == "Backspace" || event.key == "Delete") return;
+
+    switch (event.target.value.length) {
+        case 3:
+        case 7:
+            event.target.value += ".";
+            break;
+        case 11:
+            event.target.value += "-";
+            break;
+    }
+}
+
+celular.onkeyup = function (event) {
+    if (event.key == "Backspace" || event.key == "Delete") return;
+
+    if (event.key != "(" && event.key != "Shift" && event.key != ")" && event.key != "-") {
+        switch(event.target.value.length) {
+        case 1:
+            event.target.value = "(" + event.target.value;
+            break;
+        case 4:
+            event.target.value += ") ";
+            break;
+        case 10:
+            event.target.value += "-";
+            break;
+        case 16:
+            event.target.value = event.target.value.substring(0, 5)
+                + " " + event.target.value[6]
+                + " " + event.target.value.substring(7, 10)
+                + event.target.value[11]
+                + "-" + event.target.value.substring(12, 16);
+            break;
+        }
+    }
+}
+
+cep.onkeyup = function (event) {
+    if (event.key == "Backspace" || event.key == "Delete") return;
+
+    if (event.target.value.length == 5)
+        event.target.value += "-";
+}
+
+document.onclick = function (event) {
+    labelClicavel(event);
+}
